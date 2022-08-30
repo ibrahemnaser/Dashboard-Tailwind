e84501abc2d94caf33c0f4510e93f4387510b693
ibrahemnaser <i.naser4040@gmail.com>
2022-08-30 16:35:39 +0200

    Update Modal

--- scripts/helpers/toggler.js
+++ scripts/helpers/toggler.js
@@ -1,8 +1,17 @@
 const togglerIcon = document.getElementById("toggler");
 const sideBar = document.getElementById("sidebar");
+const modal = document.querySelector(".modal");
 export function showSidebar() {
   togglerIcon.addEventListener("click", () => {
-    togglerIcon.classList.toggle("toggled");
-    sideBar.classList.toggle("left-[-100%]");
+    modal.classList.toggle("opacity-50");
+    modal.classList.toggle("opacity-0");
+    modal.classList.toggle("h-screen");
+    sideBar.classList.toggle("show");
+  });
+  modal.addEventListener("click", () => {
+    sideBar.classList.remove("show");
+    modal.classList.remove("opacity-50");
+    modal.classList.add("opacity-0");
+    modal.classList.remove("h-screen");
   });
 }
